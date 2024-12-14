d38f131e72a259dc3cd359c9b79745e0967db68a
Belal Abd El Aal Ahmed Darwish <belal.darwish05@eng-st.cu.edu.eg>
2024-12-14 18:13:35 +0200

    Card twelve (#6)
    * feat: add CardNine.h and CardNine.cpp
    
    * fix: typo in GetString
    
    * feat: add class CardTwelve
    
    * feat: copy code from CardTen
    
    * feat: copy code from CardTen

---
+++ CardTwelve.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Card.h"
+#include "Cell.h"
+
+class CardTwelve : public Card
+{
+	int CardPrice;
+	int FeestoPay;
+
+public:
+
+	void ReadCardParameters(Grid* pGrid);
+
+	void Apply(Grid* pGrid, Player* pPlayer);
+};
+
