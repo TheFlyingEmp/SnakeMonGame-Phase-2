dc22a0b96f6327d3a54b8463b41041087dc20646
TheFlyingEmp <belal.darwish05@eng-st.cu.edu.eg>
2024-12-14 15:57:21 +0200

    feat: copy code from CardTen

--- CardEleven.h
+++ CardEleven.h
@@ -1,7 +1,15 @@
 #pragma once
 #include "Card.h"
+#include "Cell.h"
+
 class CardEleven : public Card
 {
+	int CardPrice;
+	int FeestoPay;
 
+public:
+
+	void ReadCardParameters(Grid* pGrid);
+
+	void Apply(Grid* pGrid, Player* pPlayer);
 };
-
