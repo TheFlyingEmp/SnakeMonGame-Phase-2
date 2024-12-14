b5f0649095c032b18e7acfc55dd9e07c08f150b8
TheFlyingEmp <belal.darwish05@eng-st.cu.edu.eg>
2024-12-14 11:20:58 +0200

    feat: add functionality of ReadCardParameters

--- CardTen.cpp
+++ CardTen.cpp
@@ -1,1 +1,16 @@
 #include "CardTen.h"
+
+void CardTen::ReadCardParameters(Grid* pGrid)
+{
+	Input* pIn = pGrid->GetInput();
+	Output* pOut = pGrid->GetOutput();
+
+	pGrid->PrintErrorMessage("Please, input the Card's Price...");
+
+	CardPrice = pIn->GetInteger(pOut);
+
+	pGrid->PrintErrorMessage("Please, input the Fees to Pay when passing the Card...");
+
+	FeestoPay = pIn->GetInteger(pOut);
+}
+
