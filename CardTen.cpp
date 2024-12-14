f5cfc3f1c958f8304f44740b74fef50bfb6530c1
TheFlyingEmp <belal.darwish05@eng-st.cu.edu.eg>
2024-12-14 12:54:18 +0200

    feat: add functionality of apply without ownership

--- CardTen.cpp
+++ CardTen.cpp
@@ -14,3 +14,27 @@ void CardTen::ReadCardParameters(Grid* pGrid)
 	FeestoPay = pIn->GetInteger(pOut);
 }
 
+void CardTen::Apply(Grid* pGrid, Player* pPlayer)
+{
+	char answer;
+	int EF = 0;
+
+	Input* pIn = pGrid->GetInput();
+	Output* pOut = pGrid->GetOutput();
+
+	do {
+		if (EF > 0)
+			pGrid->PrintErrorMessage("Please an appropriate value...");
+
+		pGrid->PrintErrorMessage("Do you want to buy this card and all other cards of corresponding number? (y/n) Card Price: " + to_string(CardPrice) + " Fees that other players will pay: " + to_string(FeestoPay));
+		answer = pIn->GetString(pOut);
+		answer = tolower(answer);
+		EF++
+	} while (!(answer == 'y' || answer == 'n'));
+
+	if (answer == 'y') {
+		pPlayer->SetWallet(pPlayer->GetWallet() - CardPrice);
+
+	}
+	else return;
+}
